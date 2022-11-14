42422be3380a23a28a963c3de1684fa300a6b9a9
Hung Duong Quang <qhungg289@protonmail.com>
2022-11-14 10:54:34 +0700

    refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
 	import { onDestroy, onMount } from "svelte";
-	import Button from "./libs/Button.svelte";
 	import { create2dArray } from "./utils/array";
+	import Button from "./libs/Button.svelte";
+	import StatsMonitor from "./libs/StatsMonitor.svelte";
 
 	let grid = [];
 	let rows = 60;
@@ -150,19 +151,8 @@
 		class="w-full md:w-auto px-4 md:px-0 flex md:flex-col items-start md:items-center gap-12"
 	>
 		<h1 class="hidden md:block text-2xl font-bold">Game of Life</h1>
-		<div
-			class="flex flex-col items-center gap-2 w-full py-2 px-4 rounded-lg bg-gray-200 dark:bg-gray-800"
-		>
-			<p class="flex justify-between gap-2 w-full">
-				Gen <span class="font-semibold">{gen}</span>
-			</p>
-			<p class="flex justify-between gap-2 w-full">
-				Alive <span class="font-semibold">{alive}</span>
-			</p>
-			<p class="flex justify-between gap-2 w-full">
-				Death <span class="font-semibold">{death}</span>
-			</p>
-		</div>
+
+		<StatsMonitor {gen} {alive} {death} />
 
 		<div class="flex flex-col gap-2 w-full">
 			<!-- Toggle the running state of the game -->
---
+++ src/libs/StatsMonitor.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let gen;
+	export let alive;
+	export let death;
+</script>
+
+<div
+	class="flex flex-col items-center gap-2 w-full py-2 px-4 rounded-lg bg-gray-200 dark:bg-gray-800"
+>
+	<p class="flex justify-between gap-2 w-full">
+		Gen <span class="font-semibold">{gen}</span>
+	</p>
+	<p class="flex justify-between gap-2 w-full">
+		Alive <span class="font-semibold">{alive}</span>
+	</p>
+	<p class="flex justify-between gap-2 w-full">
+		Death <span class="font-semibold">{death}</span>
+	</p>
+</div>
